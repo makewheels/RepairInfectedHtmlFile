95cfdb3b163877149c5935fc833b0dc6f98bd324
spring <spring@qbserver.cn>
2019-01-20 01:03:59 +0800

    D:\\Postgraduate
    Change-Id: I773836aa98eac5558da833e24957eca1ef9e4834
    Signed-off-by: spring <spring@qbserver.cn>

--- src/main/java/run/FindAllInfectedHtml.java
+++ src/main/java/run/FindAllInfectedHtml.java
@@ -25,7 +25,7 @@ public class FindAllInfectedHtml {
 
 	public static void main(String[] args) throws IOException {
 		// 目录
-		File folder = new File("D:\\Programming");
+		File folder = new File("D:\\Postgraduate");
 		// 找文件
 		Collection<File> htmlFiles = FileUtils.listFiles(folder, new IOFileFilter() {
 			@Override
